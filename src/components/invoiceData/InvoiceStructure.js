876b889759ead88f168089a80df60c09f3025d23
vishvanath surwshe <vsurwshe@gmail.com>
2020-04-28 18:54:45 +0530

    0.1.22: Modify invoice form

--- src/components/invoiceData/InvoiceStructure.js
+++ src/components/invoiceData/InvoiceStructure.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
 import InvoiceDataTable from "./invoiceTable/InvoiceDataTabel";
+import InvoiceFrom from './invoiceOperation/InvoiceFrom';
 
 class InvoiceStructure extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            setInvoiceForm:false
+        }
+    }
     
-    
+    setInvoiceForm=()=>{
+        this.setState({setInvoiceForm: !this.state.setInvoiceForm})
+    }
     
     render() { 
-        return this.loadInvoiceFrame();
+        const {setInvoiceForm}=this.state
+        return setInvoiceForm ? <InvoiceFrom cancleLoading={this.setInvoiceForm} /> : this.loadInvoiceFrame();
     }
 
     loadInvoiceFrame=()=><>
-    <Button style={{ float: "right" ,color: "#060606", backgroundColor: "#4dd814", borderColor: " #51f704" }} onClick={() => this.setLoadPayment()}>Add Payment</Button><br />
+    <Button style={{ float: "right" ,color: "#060606", backgroundColor: "#4dd814", borderColor: " #51f704" }} 
+    onClick={() => this.setInvoiceForm()}>Generate Invoices</Button><br />
     <br />
     <InvoiceDataTable
       handelEditClick={this.handelEditClick}
